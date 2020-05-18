e72b36b35158b2fd7e7f3b717d353e7eeca746a1
Francois D'Agostini <francois.dagostini@gmail.com>
2020-05-18 22:29:09 +0200

    first commité

---
+++ src/main/java/tacos/Taco.java
@@ -0,0 +1,27 @@
+package tacos;
+
+import java.util.Date;
+import java.util.List;
+
+// end::allButValidation[]
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+// tag::allButValidation[]
+import lombok.Data;
+
+@Data
+public class Taco {
+	
+	private Long id;;
+	
+	private Date createdAt;
+
+	@NotNull
+	@Size(min = 5, message = "Name must be at least 5 characters long")
+	private String name;
+	
+	@Size(min=1, message="You must choose at least 1 ingredient")
+	private List<String> ingredients;
+
+}
---
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -0,0 +1,68 @@
+package tacos.web;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import lombok.extern.slf4j.Slf4j;
+import tacos.Ingredient;
+import tacos.Ingredient.Type;
+import tacos.Taco;
+import tacos.data.IngredientRepository;
+
+@Slf4j
+@Controller
+@RequestMapping("/design")
+@SessionAttributes("order")
+public class DesignTacoController {
+
+	private final IngredientRepository ingredientRepo;
+
+	@Autowired
+	public DesignTacoController(IngredientRepository ingredientRepo) {
+		this.ingredientRepo = ingredientRepo;
+	}
+
+	@GetMapping
+	public String showDesignForm(Model model) {
+		List<Ingredient> ingredients = new ArrayList<>();
+		ingredientRepo.findAll().forEach(i -> ingredients.add(i));
+		
+		
+		Type[] types = Ingredient.Type.values();
+		for (Type type : types) {
+			model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
+		}
+
+		model.addAttribute("design", new Taco());
+		return "design";
+	}
+
+	@PostMapping
+	public String processDesign(@Valid Taco design, Errors errors) {
+		if (errors.hasErrors()) {
+			return "design";
+		}
+
+		// Save the taco design...
+		// We'll do this in chapter 3
+		log.info("Processing design: " + design);
+
+		return "redirect:/orders/current";
+	}
+
+	private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
+		return ingredients.stream().filter(x -> x.getType().equals(type)).collect(Collectors.toList());
+	}
+}
